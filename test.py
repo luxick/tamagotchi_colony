0c6e2cbdf23315447459405ad2588a7b26c3a408
luxick <git@luxick.com>
2015-07-26 21:06:27 +0200

    new curses interface for testing

--- test.py
+++ test.py
@@ -3,6 +3,7 @@
 from mayorgotchi import Mayorgotchi
 from util import Util
 import time
+import curses
 
 
 # Temporary test section
@@ -18,11 +19,24 @@ for n in range(0,50):
     list.append(Util().make_Tamagotchi())
 
 mayor = Mayorgotchi(list)
+
+
+screen = curses.initscr()
+curses.noecho()
+curses.curs_set(0)
+screen.keypad(1)
+screen.nodelay(1)
+
 for n in range (0, ticks):
+    screen.move(0,0)
     mayor.step()
-    print(chr(27) + "[2J")
-    print '----------------------After '+str(n)+' ticks------------------------------\n'
+    screen.addstr('----------------------After '+str(n)+' ticks------------------------------\n')
+    screen.addstr(mayor.give_status(show_pct))
 
-    print mayor.give_status(show_pct)
-
-    time.sleep(0.5)
+    screen.clrtobot()
+    key = screen.getch()
+    if key == ord('q'):
+        break
+    elif key < 0:
+        time.sleep(0.5)
+curses.endwin()
